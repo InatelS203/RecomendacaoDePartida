547ec3d02513d43307e82c32308dd7a7ebfe6f7e
romulobeluco <romulocoutinhobeluco@gmail.com>
2024-11-05 19:44:07 -0300

    main

--- recomendacao/models.py
+++ recomendacao/models.py
@@ -5,8 +5,11 @@ from django.db import models
 
 class player(models.Model):
     nome = models.CharField(max_length=100),
-    id = models.IntegerField(primary_key=True),
+    id = models.AutoField(primary_key=True),
     rank = models.IntegerField(),
     
     def __str__(self):
         return self.nome, self.id, self.rank
+            
+
+
