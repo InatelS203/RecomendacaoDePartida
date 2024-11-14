e0f213d9c5986a337cb13d4ae57abe2f95dc6235
romulobeluco <romulocoutinhobeluco@gmail.com>
2024-11-14 10:07:58 -0300

    codigo

--- S203L4/settings.py
+++ S203L4/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recomendacao.apps.RecomendacaoConfig',
-    'main.apps.MainConfig',
+        
 ]
 
 MIDDLEWARE = [
--- recomendacao/models.py
+++ recomendacao/models.py
@@ -4,12 +4,18 @@ from django.db import models
 
 
 class player(models.Model):
-    nome = models.CharField(max_length=100),
-    id = models.AutoField(primary_key=True),
-    rank = models.IntegerField(),
+    nome = models.CharField(max_length=10,default="Usuario")
+    id = models.AutoField(primary_key=True)
+    rank = models.IntegerField(default=0)
     
     def __str__(self):
         return self.nome, self.id, self.rank
             
+class partida(models.Model):
+    player = models.ForeignKey(player,on_delete=models.CASCADE)
+    complete = models.BooleanField()
+    text= models.CharField(max_length=300)
 
-
+    def __str__(self):
+        return self.text
+    
